8d45d779f2f3e74fddac67ae43329b7374cb76a5
cloudXA <zming794@gmail.com>
2020-01-17 11:09:57 +0800

    在模块系统中局部注册

---
+++ src/ComponentB.vue
@@ -0,0 +1,16 @@
+
+<script>
+    // 将ComponentA 和 ComponentC 导入到ComponetB模块中使用 
+
+    import ComponentA from './ComponentA.vue'
+    import ComponentC from './ComponentC.vue'
+
+
+    export default {
+        components: {
+            ComponentA,
+            ComponentC
+        }
+    }
+</script>
+
